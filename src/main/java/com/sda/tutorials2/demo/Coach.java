d2ffb08f7b288cdf5472316d23809c4ccc856351
kaiapalm <palmkaia@gmail.com>
2019-08-18 12:35:41 +0300

    Coach and Tenniscoach updated

--- src/main/java/com/sda/tutorials2/demo/Coach.java
+++ src/main/java/com/sda/tutorials2/demo/Coach.java
@@ -3,4 +3,6 @@ package com.sda.tutorials2.demo;
 public interface Coach {
 
     String getDailyWorkout();
+
+    String getDailyDiet();
 }
--- src/main/java/com/sda/tutorials2/demo/TennisCoach.java
+++ src/main/java/com/sda/tutorials2/demo/TennisCoach.java
@@ -1,13 +1,31 @@
 package com.sda.tutorials2.demo;
 
 
+import com.sda.tutorials2.demo.service.DietService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach{
 
+
+    // injecting dependency:
+
+    private DietService dietService;
+
+    @Autowired
+    public TennisCoach(DietService dietService) {
+        this.dietService = dietService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Tennis coach";
+    }
+
+
+    @Override
+    public String getDailyDiet(){
+        return dietService.getDailyDiet();
     }
 }
